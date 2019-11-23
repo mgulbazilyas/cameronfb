b8a6b0b34fc1fbcbe9a996925a498e8618838d97
Gulbaz <mgulbazilyas@gmail.com>
2019-11-24 04:00:03 +0500

    First Commit

--- group_crawler.py
+++ group_crawler.py
@@ -145,7 +145,7 @@ self = Setup()
 connection = mysqldata.con('')
 groups = connection.get_groups()
 
-for link in groups[:1]:
+for link in groups:
 
 	self.crawl_group(link)
 	print("Get data Successful from\t", link)
