f2de595d45f5cd2cdbc479a1564a6cd7a9eef127
Gulbaz <mgulbazilyas@gmail.com>
2019-11-26 00:20:10 +0500

    First Commit

--- group_crawler.py
+++ group_crawler.py
@@ -27,7 +27,7 @@ chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("start-maximized")
 # chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument('headless')
+chrome_options.add_argument('headless')
 # In[2]
 def extract_email(line):
 	try:
